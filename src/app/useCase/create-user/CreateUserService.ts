9ad7b8ddee5f0555c1feb5bffecce15f9f3f87bc
mateusVedoy <mateusvedoy@outlook.com>
2022-07-24 21:50:28 -0300

    Implementação de hash com bcryptjs no caso uso de criação de usuário

--- src/app/useCase/create-user/CreateUserService.ts
+++ src/app/useCase/create-user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { IHashPass } from "app/ports/adapter/helper/IHashPass";
 import { ICreateUserRepository } from "../../../app/ports/adapter/repository/ICreateUserRepository";
 import { ICreateUser } from "../../../app/ports/useCase/ICreateUser";
 import { User } from "../../../domain/entity/User";
@@ -5,14 +6,17 @@ import { User } from "../../../domain/entity/User";
 export class CreateUserService implements ICreateUser {
 
     private CreateUserRepository: ICreateUserRepository;
+    private HashUserPass: IHashPass;
 
-    public constructor(createUserRepository: ICreateUserRepository) {
+    public constructor(createUserRepository: ICreateUserRepository, hashUserPass: IHashPass) {
         this.CreateUserRepository = createUserRepository;
+        this.HashUserPass = hashUserPass;
     }
 
     public async create(username: string, email: string, password: string): Promise<string> {
         try {
-            const user = new User(username, email, password);
+            const hashedPass = await this.hashUserPassword(password);
+            const user = new User(username, email, hashedPass);
             return await this.sendCreatedUserToRepository(user);
         } catch (error) {
             throw error;
@@ -26,5 +30,9 @@ export class CreateUserService implements ICreateUser {
         } catch (error) {
             throw error;
         }
+    }
+
+    private async hashUserPassword(pass: string): Promise<string> {
+        return await this.HashUserPass.hash(pass);
     }
 }
--- src/main/in-memory/CreateUserMain.ts
+++ src/main/in-memory/CreateUserMain.ts
@@ -1,9 +1,11 @@
 import { CreateUserRepository } from "../../adapter/repository/User/in-memory/CreateUserRepository";
 import { CreateUserService } from "../../app/useCase/create-user/CreateUserService";
 import { CreateUserController } from "../../adapter/controller/User/CreateUserController";
+import { BcryptJsPassHash } from "../../adapter/helper/BcryptJsPassHash";
 
 const createUserRepo = new CreateUserRepository();
-const createUserService = new CreateUserService(createUserRepo);
+const bcryptJsPassHash = new BcryptJsPassHash();
+const createUserService = new CreateUserService(createUserRepo, bcryptJsPassHash);
 const createUserControl = new CreateUserController(createUserService);
 
 export {
