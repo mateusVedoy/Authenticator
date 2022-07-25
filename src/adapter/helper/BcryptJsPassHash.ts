b1b19616024dd616b78a238ec77975797eef0596
mateusVedoy <mateusvedoy@outlook.com>
2022-07-24 23:06:29 -0300

    Implantação de gerador de hash bryptjs

---
+++ src/adapter/helper/BcryptJsPassHash.ts
@@ -0,0 +1,9 @@
+import { IHashPass } from "app/ports/adapter/helper/IHashPass";
+import bcryptjs from "bcryptjs"
+
+export class BcryptJsPassHash implements IHashPass {
+    public async hash(planTextPass: string): Promise<string> {
+        const salt = await bcryptjs.genSalt(10);
+        return bcryptjs.hash(planTextPass, salt);
+    }
+}
